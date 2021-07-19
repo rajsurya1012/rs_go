58d9c7a5f6aadb23e239fc358cb3aabc6bae4c54
Raj Surya <42248521+rajsurya1012@users.noreply.github.com>
2021-07-20 01:29:45 +0530

    Add files via upload

---
+++ src/laser_readings.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+import rospy
+from sensor_msgs.msg import LaserScan
+
+def callback_laser(msg):
+    regions=[
+        min(min(msg.ranges[0:143]),10),
+        min(min(msg.ranges[144:287]),10),
+        min(min(msg.ranges[288:431]),10),
+        min(min(msg.ranges[432:575]),10),
+        min(min(msg.ranges[576:713]),10),
+    ]
+    rospy.loginfo(regions)
+
+def main():
+    rospy.init_node('laser_readings')
+    sub=rospy.Subscriber("/rs_go_description/laser/scan", LaserScan, callback_laser)
+    rospy.spin()
+
+if __name__=='__main__':
+    main()
